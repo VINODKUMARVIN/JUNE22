3f9fc966ab40dbe3e2965534929f2586a49162ca
VINODKUMARVIN <mvinodmech2021@gmail.com>
2022-06-06 13:26:43 +0530

    SEVENTH PUSH

---
+++ src/com/vinod/ifstatement/Ifprogram.java
@@ -0,0 +1,16 @@
+package com.vinod.ifstatement;
+
+public class Ifprogram {
+	public static void main(String[] args) {
+	System.out.println("program starts");
+	int marks=90;
+	int passingmarks=80;
+	if(marks>=passingmarks)
+	{
+		System.out.println("result:Pass");
+	}
+	System.out.println("program ends");
+	}
+	}
+
+
